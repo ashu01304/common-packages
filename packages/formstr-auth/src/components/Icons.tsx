4a731b9acac3b5b7ba1cb50aa3f2216f02335c17
Ashutosh <aashutoshgandhi13@gmail.com>
2026-04-09 03:28:08 +0530

    .

--- packages/formstr-auth/src/components/Icons.tsx
+++ packages/formstr-auth/src/components/Icons.tsx
@@ -1,43 +1,43 @@
 import React from "react";
 
-export const VpnKeyIcon = ({ className = "" }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+export const VpnKeyIcon = ({ className = "", style = {} }) => (
+  <svg style={style} className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
     <path d="M12.65 10C11.83 7.67 9.61 6 7 6c-3.31 0-6 2.69-6 6s2.69 6 6 6c2.61 0 4.83-1.67 5.65-4H17v4h4v-4h2v-4H12.65zM7 14c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z" />
   </svg>
 );
 
-export const HubIcon = ({ className = "" }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+export const HubIcon = ({ className = "", style = {} }) => (
+  <svg style={style} className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
     <path d="M20 16c-1.57 0-2.91-1-3.41-2.4l-2.48-1.24c.24-.55.39-1.15.39-1.76 0-1.1-.47-2.09-1.22-2.8L16.59 5.4C17.41 4.56 18.63 4 20 4c2.76 0 5 2.24 5 5s-2.24 5-5 5zm0-8c-1.65 0-3 1.35-3 3s1.35 3 3 3 3-1.35 3-3-1.35-3-3-3zM4 14c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm0-8c1.65 0 3 1.35 3 3s-1.35 3-3 3-3-1.35-3-3 1.35-3 3-3zm8 16c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm0-8c1.65 0 3 1.35 3 3s-1.35 3-3 3-3-1.35-3-3 1.35-3 3-3z" />
   </svg>
 );
 
-export const PersonIcon = ({ className = "" }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+export const PersonIcon = ({ className = "", style = {} }) => (
+  <svg style={style} className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
     <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
   </svg>
 );
 
-export const PhonelinkLockIcon = ({ className = "" }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+export const PhonelinkLockIcon = ({ className = "", style = {} }) => (
+  <svg style={style} className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
     <path d="M19 1H9c-1.1 0-2 .9-2 2v3h2V4h10v16H9v-2H7v3c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V3c0-1.1-.9-2-2-2zM7.01 13.47l-2.55-2.55-1.27 1.27L7.01 16 14.5 8.51l-1.27-1.27-6.22 6.23z" />
   </svg>
 );
 
-export const ChevronRightIcon = ({ className = "" }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+export const ChevronRightIcon = ({ className = "", style = {} }) => (
+  <svg style={style} className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
     <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
   </svg>
 );
 
-export const ContentCopyIcon = ({ className = "" }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+export const ContentCopyIcon = ({ className = "", style = {} }) => (
+  <svg style={style} className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
     <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
   </svg>
 );
 
-export const LogoutIcon = ({ className = "" }) => (
-  <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+export const LogoutIcon = ({ className = "", style = {} }) => (
+  <svg style={style} className={className} width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
     <path d="M13 3H3v18h10v-2H5V5h8V3zm10 9l-4-4v3H7v2h12v3l4-4z" />
   </svg>
 );
